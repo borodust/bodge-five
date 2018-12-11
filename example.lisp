98e2a0f804d5837e707f3e92163bcd817c525439
Pavel Korolev <dev@borodust.org>
2018-12-11 22:55:39 +0300

    Fix example and bump libfive

--- example.lisp
+++ example.lisp
@@ -3,9 +3,9 @@
   (:export #:render))
 (cl:in-package :libfive.example)
 
-;; Adapted from "libfive_tree_render_slice" test case
-(defun render (&optional (svg-path (asdf:system-relative-pathname
-                                    :bodge-five/example "local/circle.svg")))
+;; Adapted from "libfive_tree_render_mesh" test case
+(defun render (&optional (output-path (asdf:system-relative-pathname
+                                       :bodge-five/example "local/result.stl")))
   (libfive:init (bodge-blobs-support:find-loaded-library-name :five-blob :main))
   (claw:with-float-traps-masked ()
     ;; All of these trees are manually managed and must be freed
@@ -13,31 +13,44 @@
     ;; destructor and run during garbage collection.
     (let* ((x (%libfive:tree-x))
            (y (%libfive:tree-y))
+           (z (%libfive:tree-z))
 
            (x2 (%libfive:tree-unary (%libfive:opcode-enum "square") x))
            (y2 (%libfive:tree-unary (%libfive:opcode-enum "square") y))
-           (r (%libfive:tree-binary (%libfive:opcode-enum "add") x2 y2))
+           (z2 (%libfive:tree-unary (%libfive:opcode-enum "square") z))
+
+           (r- (%libfive:tree-binary (%libfive:opcode-enum "add") x2 y2))
+           (r (%libfive:tree-binary (%libfive:opcode-enum "add") r- z2))
 
            (one (%libfive:tree-const 1f0))
            (d (%libfive:tree-binary (%libfive:opcode-enum "sub") r one)))
 
-      ;; Select a 2D region to export, then write an SVG
+      ;; Select a 3D region to render
+      (claw:c-with ((region %libfive:region3))
+        (setf (region :x :lower) -2f0
+              (region :x :upper) 2f0
+              (region :y :lower) -2f0
+              (region :y :upper) 2f0
+              (region :z :lower) -2f0
+              (region :z :upper) 2f0)
+        (let ((tree-str (%libfive:tree-print d)))
+          (format t "~&Tree:~&~A"
+                  (cffi:foreign-string-to-lisp tree-str))
+          (finish-output)
+          (claw:free tree-str))
 
-      (claw:c-with ((region %libfive:region2))
-        (setf (region :x :lower -2)
-              (region :x :upper 2)
-              (region :y :lower -2)
-              (region :y :upper 2))
-        (let ((cs (%libfive:tree-render-slice d region 0f0 10f0)))
-          (ensure-directories-exist svg-path)
-          (%libfive:tree-save-slice d region 0f0 10f0 (namestring svg-path))
+        (ensure-directories-exist output-path)
+        (%libfive:tree-save-mesh d region 100f0 (namestring output-path))
+        (format t "~&Rendering finished: ~A" output-path)
 
-          ;; Finally, clean up all of the intermediate trees
-          (%libfive:tree-delete x)
-          (%libfive:tree-delete y)
-          (%libfive:tree-delete x2)
-          (%libfive:tree-delete y2)
-          (%libfive:tree-delete r)
-          (%libfive:tree-delete one)
-          (%libfive:tree-delete d)
-          (%libfive:contours-delete cs))))))
+        ;; Finally, clean up all of the intermediate trees
+        (%libfive:tree-delete x)
+        (%libfive:tree-delete y)
+        (%libfive:tree-delete z)
+        (%libfive:tree-delete x2)
+        (%libfive:tree-delete y2)
+        (%libfive:tree-delete z2)
+        (%libfive:tree-delete r-)
+        (%libfive:tree-delete r)
+        (%libfive:tree-delete one)
+        (%libfive:tree-delete d)))))
